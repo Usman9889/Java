9ba0785395f5535cd463c6e2816519b8a40a28bf
Mohd Usman <usman9889a@gmail.com>
2025-02-22 11:35:14 +0530

    Basics

---
+++ Day1/JavaBasics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class JavaBasics {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // float rad = sc.nextFloat();
+        // float area = 3.14f * rad * rad ;
+        // System.out.println("Area of a cirle with radius "+rad+" is: "+area);
+        char ch = 'Z';
+        int number = ch;
+        System.out.println(number);
+
+
+    }
+
+}
