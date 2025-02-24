6700e2fc962d1f44478b2d9bcf877c23a043bd7a
Mohd Usman <usman9889a@gmail.com>
2025-02-24 09:39:16 +0530

    binary to decimal

---
+++ Functions/binToDec.java
@@ -0,0 +1,20 @@
+package Functions;
+
+public class binToDec {
+      public static void binToDec(int binNum){
+            int myNum = binNum;
+            int dec = 0;
+            int pow = 0;
+            while(binNum > 0){
+                  int lastDigit = binNum % 10;
+                  dec = dec + (lastDigit * (int)Math.pow(2, pow));
+
+                  pow ++;
+                  binNum = binNum / 10;
+            }
+            System.out.println("deicamal of " + myNum + " is " + dec);
+      }
+      public static void main(String args[]) {
+            binToDec(101);
+      }
+}
