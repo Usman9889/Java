ad948c6d64d5859603892752aa1892231d12f85a
Usman9889 <usman9889a@gmail.com>
2025-03-19 12:20:33 +0530

    kadanes max sub array

---
+++ Array/kadanesMaxSubArray.java
@@ -0,0 +1,22 @@
+public class kadanesMaxSubArray {
+      
+      public static void kadanes(int numbers[]){
+
+            int currSum = 0;
+            int maxSum = Integer.MIN_VALUE;
+            
+            for(int i=0; i<numbers.length; i++){
+                  currSum = currSum + numbers[i];
+                  if(currSum < 0){
+                        currSum = 0;
+                  }
+                  maxSum = Math.max(currSum, maxSum);
+            }
+            System.out.println(" max sum is: " + maxSum);
+      }
+
+      public static void main(String args[]){
+            int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3 };
+            kadanes(numbers);
+      }
+}
